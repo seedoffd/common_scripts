e5592acc1e53575eb2db601e9da92bc8a98fb091
Farkhod <sadykovfarkhod@gmail.com>
2019-07-25 17:17:40 -0500

    Script to get all list of groups from stash

---
+++ stashGetUsers/scrtip.py
@@ -0,0 +1,17 @@
+import os
+import stashy
+import json
+
+stash_url = os.environ.get('STASH_URL')
+username  = os.environ.get('STASH_ADMIN_USER')
+password  = os.environ.get('STASH_ADMIN_PASSWORD')
+
+if username and password and stash_url:
+    stash = stashy.connect(f"{stash_url}", username, password)
+else:
+    print('PLease configure <STASH_URL>, <STASH_ADMIN_USER>, <STASH_ADMIN_PASSWORD>')
+
+
+
+with open('groups.json', 'w') as file:
+    json.dump(stash.admin.groups.list(), file, indent=2)
---
+++ user-migration/get-existing-users.py
@@ -0,0 +1,18 @@
+import json
+from app import User
+
+output_users = []
+data = User.query.all()
+
+for user in data:
+    output_users.append(
+      {
+        "firstname" : user.firstname,
+        "lastname"  : user.lastname,
+        "username"  : user.username,
+        "password"  : user.password,
+        "email"     : user.email,
+        "role"      : user.role
+      })
+
+print(json.dumps(output_users, indent=2))
