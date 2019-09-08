2da6e39d8af0a75571e2299daee981de56cb3b83
Farkhod <sadykovfarkhod@gmail.com>
2019-09-08 13:37:48 -0500

    Created common_script for deployments

--- set-environments/terraform/google-set-env.sh
+++ set-environments/terraform/google-set-env.sh
@@ -7,17 +7,20 @@ DATAFILE="$DIR/$1"
 # these all variables should be created on your config file before you run script.
 # <ENVIRONMENT> <BUCKET> <DEPLOYMENT> <PROJECT> <CREDENTIALS>
 
-ENVIRONMENT=$(sed -nr 's/^environment\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
-BUCKET=$(sed -nr 's/^bucket_name\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
-DEPLOYMENT=$(sed -nr 's/^deployment_name\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
-PROJECT=$(sed -nr 's/^project\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
-CREDENTIALS=$(sed -nr 's/^credentials\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
-
-
 if [ ! -f "$DATAFILE" ]; then
-    echo "setenv: Configuration file not found: $DATAFILE"
-    return 1
+  echo "setenv: Configuration file not found: $DATAFILE"
+  return 1
 fi
+
+wget --quiet -O "$PWD/common_configuration.tfvars"\
+  "https://raw.githubusercontent.com/fuchicorp/main-fuchicorp/master/project-configuration/google_account_information.tfvars"
+
+
+BUCKET=$(sed -nr 's/^google_bucket_name\s*=\s*"([^"]*)".*$/\1/p' "$PWD/common_configuration.tfvars")
+PROJECT=$(sed -nr 's/^google_project_id\s*=\s*"([^"]*)".*$/\1/p' "$PWD/common_configuration.tfvars")
+ENVIRONMENT=$(sed -nr 's/^environment\s*=\s*"([^"]*)".*$/\1/p'    "$DATAFILE")
+DEPLOYMENT=$(sed -nr 's/^deployment_name\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
+CREDENTIALS=$(sed -nr 's/^credentials\s*=\s*"([^"]*)".*$/\1/p'    "$DATAFILE")
 
 if [ -z "$ENVIRONMENT" ]
 then
@@ -27,13 +30,13 @@ fi
 
 if [ -z "$BUCKET" ]
 then
-  echo "setenv: 'bucket_name' variable not set in configuration file."
+  echo "setenv: 'google_bucket_name' variable not set in configuration file."
   return 1
 fi
 
 if [ -z "$PROJECT" ]
 then
-    echo "setenv: 'project_name' variable not set in configuration file."
+    echo "setenv: 'google_project_id' variable not set in configuration file."
     return 1
 fi
 
@@ -59,12 +62,10 @@ terraform {
 }
 EOF
 
-
 GOOGLE_APPLICATION_CREDENTIALS="${DIR}/${CREDENTIALS}"
-
 export GOOGLE_APPLICATION_CREDENTIALS
 export DATAFILE
-rm -rf "$DIR/.terraform"
-
+{ rm -rf "$DIR/.terraform" 2>/dev/null } || { echo "The terraform folder does not exist!!" }
+rm -rf "$PWD/common_configuration.tfvars"
 echo "setenv: Initializing terraform"
 terraform init #> /dev/null
