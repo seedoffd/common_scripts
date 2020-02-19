b4f4e32280f32ee4cae1a58148dbd55b4daa9e41
Farkhod Sadykov <sadykovfarkhod@gmail.com>
2020-02-19 13:21:43 -0600

    Fixed print users

--- bastion-scripts/sync-users.py
+++ bastion-scripts/sync-users.py
@@ -81,17 +81,16 @@ for team in team_items:
     root_members = templetize_user_data(root_access_teams, team)
     if root_members:
         for user in root_members:
-            print(f"""###### {user["username"]} '{user["comment"]}' {user["username"]}.key --admin""")
-            # os.system(f"""sudo sh user_add.sh {user["username"]} '{user["comment"]}' {user["username"]}.key --admin""")
+            # print(f"""###### {user["username"]} '{user["comment"]}' {user["username"]}.key --admin""")
+            os.system(f"""sudo sh user_add.sh {user["username"]} '{user["comment"]}' {user["username"]}.key --admin""")
             bastion_access["root_access"].append(user)
 
     ## Getting non root members
     non_root_members = templetize_user_data(non_root_access_teams, team)
     if non_root_members:
         for user in non_root_members:
-            print(f"""###### {user["username"]} '{user["comment"]}' {user["username"]}.key """)
-            # os.system(f"""
-            # sudo sh user_add.sh {user["username"]} '{user["comment"]}' {user["username"]}.key """)
+            # print(f"""###### {user["username"]} '{user["comment"]}' {user["username"]}.key """)
+            os.system(f"""sudo sh user_add.sh {user["username"]} '{user["comment"]}' {user["username"]}.key""")
             bastion_access["non_root_access"].append(user)
 
 
