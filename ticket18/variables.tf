3c73e1549766e5d092101d22fdcce0697bf1d149
aidinkobonov <aidinkobonov@gmail.com>
2020-03-12 17:44:22 +0000

    edited tfvars files

--- ticket18/variables.tf
+++ ticket18/variables.tf
@@ -1,6 +1,6 @@
 variable "roles" {
 type = "list"
-default = ["roles/dns.admin", "roles/container.admin", "roles/compute.storageAdmin", "roles/compute.admin"]
+#default = ["roles/dns.admin", "roles/container.admin", "roles/compute.storageAdmin", "roles/compute.admin"]
 
 
 
